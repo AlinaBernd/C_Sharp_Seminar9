b2157f4b3f128d43686ba79b0673dcd6aaa16b6b
alina bernd <a-manaeva@mail.ru>
2023-09-07 15:21:18 +0300

    Добавила решение решение задачи 66

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
-void PrintNumbers(int num)
+/*void PrintNumbers(int num)
     {
         if (num >= 1)
         {
@@ -12,11 +12,27 @@ void PrintNumbers(int num)
     }
 Console.Write("Input the number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-PrintNumbers(number);
+PrintNumbers(number);*/
 
 /*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
+
+int SumNumbers(int numM, int numN)
+{
+    if (numM > numN)
+        return 0;
+    else
+        return numM + SumNumbers(numM + 1, numN);
+}
+
+Console.Write("Input the number M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = SumNumbers(num1, num2);
+Console.WriteLine($"The sum of the natural numbers from {num1} to {num2} is: {sum}");
+
 
 /*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
